ec1d1bbca3fbad9588d0015cf630bdf20f75978c
Joshua625 <joshua.sager@bellevuecollege.edu>
2025-02-24 16:54:31 -0800

    added more cubes

---
+++ Assets/Scripts/Electron_Remover.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Electron_Remover : MonoBehaviour
+{
+  
+   public void remover(GameObject thing)
+    {
+        thing = 
+       if (thing is GameObject)
+        {
+            
+            Object.Destroy(thing);
+        }
+    }
+}
