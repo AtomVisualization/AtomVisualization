3bd4160c657c4051990ccf5dba5b85c2fc4d7921
Joshua625 <joshua.sager@bellevuecollege.edu>
2025-02-27 18:33:14 -0800

    other atoms

--- Assets/Scripts/Atom_Switch.cs
+++ Assets/Scripts/Atom_Switch.cs
@@ -7,12 +7,11 @@ using UnityEngine;
 public class Atom_Switch : MonoBehaviour
 {
 
-    public string atomm = "nill";
+    public static string atomm = "nill";
     public string namee = "nill";
    public void Switch() 
    {
-    atomm = Global_Var_First.atom;
     atomm = this.name;
-    Debug.Log(atomm);
+    Debug.Log(atomm + " hi");
    }
 }
--- Assets/Scripts/Global_Var_First.cs
+++ Assets/Scripts/Global_Var_First.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Global_Var_First : MonoBehaviour
 {
     public static int elec1 = 0;
-    public static string atom = "nill";
+    public static int elec2 = 0;
+    public static int elec3 = 0;
+   
 
     public void increment() 
     {
@@ -15,5 +17,25 @@ public class Global_Var_First : MonoBehaviour
     public void decrement() 
     {
         elec1--;
+    }
+
+    public void increment2() 
+    {
+        elec2++;
+    }
+
+    public void decrement2() 
+    {
+        elec2--;
+    }
+
+    public void increment3() 
+    {
+        elec3++;
+    }
+
+    public void decrement3() 
+    {
+        elec3--;
     }
 }
