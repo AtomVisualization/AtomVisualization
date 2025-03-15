0a08b08f1153e18d228f68422866045d178ab670
Joshua625 <joshua.sager@bellevuecollege.edu>
2025-03-15 15:57:45 -0700

    barely anything

---
+++ Assets/Scripts/Check_Bonds.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Check_Bonds : MonoBehaviour
+{
+    public string bond = "nill";
+   public void check1() 
+   {
+    bond = Bond_Switcher.Bondd;
+
+
+    if (bond == "NaCl") 
+    {
+        
+    }
+   }
+}
