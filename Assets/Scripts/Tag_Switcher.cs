0e000449d58bbedaa1209541f28e6d94dcf7d09f
Joshua625 <joshua.sager@bellevuecollege.edu>
2025-03-17 15:12:55 -0700

    changes

---
+++ Assets/Scripts/Tag_Switcher.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tag_Switcher : MonoBehaviour
+{
+    public static bool checkk = false;
+    private string bond = "nill";
+  public void Switcher1(Collider other)
+    {
+        bond = Bond_Switcher.Bondd;
+
+        if (bond == "H2")
+        {
+            if (other.CompareTag("Hydrogen"))
+            {
+                checkk = true;
+                Debug.Log("checkk is true");
+            }
+        }
+    }
+
+    public void Switcher2(Collider other)
+    {
+        checkk = false;
+    }
+}
