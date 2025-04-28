bb16e492ac572fa6c7015ed7916740693ec9db9f
Joshua625 <joshua.sager@bellevuecollege.edu>
2025-04-28 14:30:45 -0700

    more

--- Assets/Scripts/Clone.cs
+++ Assets/Scripts/Clone.cs
@@ -9,7 +9,7 @@ public class Clone : MonoBehaviour
     public int timesCloned;
 
 public void clone() {
-        //checks how many times the object your holding has been colend and if it's more than one the it doesn'r clone anymore
+        //checks how many times the object your holding has been cloned and if it's more than one the it doesn't clone anymore
     if (timesCloned >= 1) {
         return;
     }
